46eb39d3bc6e4931c1705461621a472a10158504
Girishgktester <girishgk.tester@gmail.com>
2026-03-03 22:25:41 +0530

    02-folder components done

---
+++ 02-folder/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Manisha</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut explicabo non illo minima natus, est voluptatem facilis! Eius, placeat. Deleniti, facere placeat dolor quis ipsum amet animi impedit vero modi.</p>      
+    </div>
+  )
+}
+
+export default Card
---
+++ 02-folder/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      Hi I am navbar
+    </div>
+  )
+}
+
+export default Navbar
